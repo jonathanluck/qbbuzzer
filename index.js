4a69d21a8e349dca7fb842131f34361dc5f086ac
Jonathan Luck <jrluck@hotmail.com>
2015-04-02 17:49:22 -0700

    Name checking
    Name checking to prevent duplicates and clear button hiding

--- index.js
+++ index.js
@@ -2,6 +2,17 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var canbuzz = true;
+var names = [""];
+function checkname(testname){
+	for (i =0;i<names.length;i++){
+		if(names[i]== testname){
+			return false;
+		}
+	}
+	return true;
+
+}
+
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
@@ -12,14 +23,30 @@ app.get('/style.css', function(req, res){
 
 io.on('connection', function(socket){
   socket.on('buzz',function(buzz){
-	socket.broadcast.emit('locked');
+	if(canbuzz){
+		socket.broadcast.emit('locked',buzz);
+		canbuzz = false
+	}
   })
   
 });
 
 io.on('connection', function(socket){
  socket.on('clear',function(buzz){
-	socket.broadcast.emit('clear');
+	socket.broadcast.emit('clear',buzz);
+	canbuzz = true
+  })
+  
+});
+
+io.on('connection', function(socket){
+ socket.on('check name',function(name){
+	if(checkname(name)){
+		names[names.length] = name;
+	}
+	else{
+		socket.emit('bad name',"");
+	}
   })
   
 });
