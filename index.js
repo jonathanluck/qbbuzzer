b16f8f57512adcb4f7aa4f25b57cb3edc12ec24a
kevinwu1 <kevinwu1997@gmail.com>
2015-04-03 17:00:32 -0700

    Working js

--- index.js
+++ index.js
@@ -4,17 +4,34 @@ var io = require('socket.io')(http);
 var canbuzz = true;
 var names = [""];
 var currentbuzzer = "";
-function checkname(testname){
+var currentusers = new Array();
 
+
+// allows server to send all files needed for the website
+files=['','index.html','style.css','pop.mp3','socketio.js','qbbuzzer.js','buzzsound.mp3'];
+files.forEach(function(a){
+	app.get('/'+a, function(req, res){
+		res.sendFile(__dirname + '/'+a);
+	});
+});
+
+
+// has the server start and listen on port 8080
+http.listen(8080, function(){
+  console.log('listening on *:8080');
+});
+
+
+// sanitizes the name then checks if it is already being used
+function checkname(testname){
+	testname = testname.trim().replace(/</g,"");
 	for (i =0;i<names.length;i++){
 		if(names[i]== testname){
 			return false;
 		}
 	}
 	return true;
-
 }
-
 
 files=['','style.css','pop.mp3','qbbuzzer.js'];
 files.forEach(function(a){
@@ -23,50 +40,75 @@ files.forEach(function(a){
 	});
 });
 
-function socketOn(str, func){
-	io.on('connection', function(socket){
-		socket.on(str,func);
-	});
-}
 
-
-socketOn('buzz',function(buzz){
-	if(canbuzz){
-		socket.broadcast.emit('locked',buzz);
-		io.sockets.connected[socket.id].emit('your buzz', buzz)
-		currentbuzzer= buzz;
-		canbuzz = false
-	}
-});
-
-socketOn('clear',function(buzz){
-	io.sockets.connected[socket.id].emit('clear',buzz);
-	socket.broadcast.emit('clear',buzz);
-	currentbuzzer="";
-	canbuzz = true
-});
-
-socketOn('check name',function(name){
-
-	if(checkname(name)){
-		names[names.length] = name;
-		io.sockets.connected[socket.id].emit('good name', name);
-		if (!canbuzz){
-			io.sockets.connected[socket.id].emit('locked', currentbuzzer);
-		}
-	}
-	else{
-		 io.sockets.connected[socket.id].emit('bad name', '');
-	}
-  });
+// recieves a buzz
+// sends a lock signal to everyone but the buzzer, who gets a signal indicating it is their buzz
 io.on('connection', function(socket){
-socket.on('disconnect', function(){
-    socket.broadcast.emit('clear');
-	canbuzz=true;
+	socket.on('buzz',function(buzz){
+		if(canbuzz){
+			socket.broadcast.emit('locked',buzz);
+			io.sockets.connected[socket.id].emit('your buzz', buzz)
+			currentbuzzer= buzz;
+			canbuzz = false
+		}
+	});
 });
+// sends a clear signal to all clients and allows anyone to buzz again
+io.on('connection', function(socket){
+	socket.on('clear',function(buzz){
+		io.sockets.connected[socket.id].emit('clear',buzz);
+		socket.broadcast.emit('clear',buzz);
+		currentbuzzer="";
+		canbuzz = true
+	});
 });
 
 
-http.listen(8080, function(){
-  console.log('listening on *:8080');
+// checks if a name is useable. locks the buzzer if someone has already buzzed. 
+// broadcasts to all clients to add the new name
+// adds all current names to new client
+// if the name is already used, then rejects the name
+io.on('connection', function(socket){
+	socket.on('check name',function(name){
+		if(checkname(name)){
+			name = name.trim().replace(/</g,"");
+			io.sockets.connected[socket.id].emit('good name', name);
+			if (!canbuzz){
+				io.sockets.connected[socket.id].emit('locked', currentbuzzer);
+			}
+			socket.broadcast.emit('add name',name);
+			names.forEach(function(a){
+				io.sockets.connected[socket.id].emit('add name', a);
+			});
+			names[names.length] = name;
+			currentusers[socket.id] = name;
+		}
+		else{
+			 io.sockets.connected[socket.id].emit('bad name', '');
+		}
+	});
+});
+  
+io.on('connection', function(socket){
+	socket.on('disconnect', function(){
+		socket.broadcast.emit('clear');
+		canbuzz=true;
+	});
+});
+
+
+// clears the buzzer when a user that has buzzed disconnects
+// sends a message to all clients telling them to remove disconnected client from their lists
+// frees up the username from the list of names
+io.on('connection', function(socket){
+	socket.on('disconnect', function(){
+		var name = currentusers[socket.id];
+		if(name == currentbuzzer){
+			socket.broadcast.emit('clear');
+			canbuzz=true;
+		}
+		socket.broadcast.emit('remove name',name);
+		delete names[names.indexOf(name)];
+		delete currentusers[socket.id];
+	});
 });
