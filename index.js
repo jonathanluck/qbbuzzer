b14b8fa964dc035f83b74c88d66ae8f7ae47e0db
kevinwu1 <kevinwu1997@gmail.com>
2015-06-27 23:50:30 -0700

    formatting

--- index.js
+++ index.js
@@ -4,31 +4,28 @@ var io = require('socket.io')(http);
 var fs = require('fs');
 var users = {};
 var rooms = {};
-var ips = {"":""};
+var ips = {"": ""};
 if (typeof __dirname == "undefined")
 	__dirname = "C:\\Program Files\\nodejs\\qbbuzzer";
 
-
 // allows server to send all files needed for the website
-files=['','index.html','style.css','pop.mp3','socketio.js','qbbuzzer.js','buzzsound.mp3'];
+files = ['', 'index.html', 'style.css', 'pop.mp3', 'socketio.js', 'qbbuzzer.js', 'buzzsound.mp3'];
 files.forEach(function(a){
-	app.get('/'+a, function(req, res){
-		res.sendFile(__dirname + '/'+a);
+	app.get('/' + a, function(req, res){
+		res.sendFile(__dirname + '/' + a);
 	});
 });
 
-
 // has the server start and listen on port 8080
 http.listen(8080, function(){
-  console.log('listening on *:8080');
+	console.log('listening on *:8080');
 });
 
-
 // sanitizes the name then checks if it is already being used for a specific room
-function checkname(testname,room){
+function checkname(testname, room){
 	testname = sanitize(testname);
 	room = sanitize(room);
-	if (room.length==0)
+	if (room.length == 0)
 		room = "default";
 	room[room]
 	return true;
@@ -36,71 +33,69 @@ function checkname(testname,room){
 function genrandomname(){
 	string = ""
 	chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ01234567890";
-	for (var i = 0; i< Math.round(Math.random()*7)+6;i++){
-		string+= chars.charAt(Math.floor(Math.random()*chars.length));
+	for (var i = 0; i < Math.round(Math.random() * 7) + 6; i++) {
+		string += chars.charAt(Math.floor(Math.random() * chars.length));
 	}
 	return string;
 
 }
-
 
 // sanitization to prevent XSS attacks
 function sanitize(string){
-	string = (string+"").trim().replace(/[<'"&;]/g,"");
-	if(string.length>60)
-		return string.substring(0,60);
+	string = (string + "").trim().replace(/[<'"&;]/g, "");
+	if (string.length > 60)
+		return string.substring(0, 60);
 	return string;
 }
 
-function addRoom(room) {
-	if(room.constructor==Room)
-		rooms[room.name]=room;
+function addRoom(room){
+	if (room.constructor == Room)
+		rooms[room.name] = room;
 }
 
-function Room(name) {
+function Room(name){
 	this.name = name;
 	this.users = [];
-	this.buzzer="";
-	this.buzz = function(name) {
-		if (this.buzzer=="") {
+	this.buzzer = "";
+	this.buzz = function(name){
+		if (this.buzzer == "") {
 			this.buzzer = name;
 			this.users.forEach(function(u){
 				if (u.name == name)
-					u.socket.emit('your buzz', name,(new Date(Date.now())+"").substring(16,24));
+					u.socket.emit('your buzz', name, (new Date(Date.now()) + "").substring(16, 24));
 				else
-					u.socket.emit('locked',name,(new Date(Date.now())+"").substring(16,24));
+					u.socket.emit('locked', name, (new Date(Date.now()) + "").substring(16, 24));
 			});
 		}
 	}
-	this.clear = function() {
+	this.clear = function(){
 		this.buzzer = "";
 		this.users.forEach(function(u){
-			u.socket.emit('clear',"")
+			u.socket.emit('clear', "")
 		});
 	}
-	this.addUser = function(user) {
+	this.addUser = function(user){
 		user.socket.emit("add names", JSON.stringify(this.users.map(function(u){
 			return u.name;
 		})));
 		this.users.forEach(function(u){
-			u.socket.emit('add names', '["'+user.name+'"]');
+			u.socket.emit('add names', '["' + user.name + '"]');
 		})
 		this.users.push(user);
 	}
-	this.removeUser = function(user) {
-		if (this.buzzer==user.name)
+	this.removeUser = function(user){
+		if (this.buzzer == user.name)
 			this.clear();
-		for (var i = 0; i < this.users.length; i++) 
-			if (this.users[i]==user)
-				this.users.splice(i,1);
+		for (var i = 0; i < this.users.length; i++)
+			if (this.users[i] == user)
+				this.users.splice(i, 1);
 		this.users.forEach(function(u){
-			u.socket.emit('remove name', user.name, (new Date(Date.now())+"").substring(16,24));
+			u.socket.emit('remove name', user.name, (new Date(Date.now()) + "").substring(16, 24));
 		})
 	}
 }
 
-
-function User(name, socketID, roomName) {
+function User(name, socketID, roomName){
 	users[socketID] = this;
 	if (typeof rooms[roomName] == "undefined")
 		addRoom(new Room(roomName));
@@ -111,52 +106,51 @@ function User(name, socketID, roomName) {
 
 io.on('connection', function(socket){
 	ips[socket.id] = socket.request.connection.remoteAddress;
-	fs.appendFile('iplog.txt',socket.request.connection.remoteAddress+"\t"+new Date(Date.now())+"\n");
+	fs.appendFile('iplog.txt', socket.request.connection.remoteAddress + "\t" + new Date(Date.now()) + "\n");
 	// recieves a buzz
 	// sends a lock signal to everyone but the buzzer, who gets a signal indicating it is their buzz
-	socket.on('buzz',function(buzz){
+	socket.on('buzz', function(buzz){
 		users[socket.id].room.buzz(users[socket.id].name);
 	});
-	
+
 	// sends a clear signal to all clients and allows anyone to buzz again
-	socket.on('clear',function(buzz){
+	socket.on('clear', function(buzz){
 		users[socket.id].room.clear();
 	});
 
-	// checks if a name is useable. locks the buzzer if someone has already buzzed. 
+	// checks if a name is useable. locks the buzzer if someone has already buzzed.
 	// broadcasts to all clients to add the new name
 	// adds all current names to new client
 	// if the name is already used, then rejects the name
-	socket.on('check name',function(name){
+	socket.on('check name', function(name){
 		name = sanitize(name);
-		if(name.length==0){
+		if (name.length == 0) {
 			name = genrandomname();
 		}
-		if(checkname(name,socket.room)){
+		if (checkname(name, socket.room)) {
 			io.sockets.connected[socket.id].emit('good name', name);
 			var user = new User(name, socket.id, socket.room);
 			user.room.addUser(user);
 		}
-		else{
-			 io.sockets.connected[socket.id].emit('bad name', '');
+		else {
+			io.sockets.connected[socket.id].emit('bad name', '');
 		}
 	});
-	
-	
-	socket.on('send room',function(room){
-			room = sanitize(room);
-			if (room.length>0){
-				socket.room = room;
-				socket.join(room);
-				io.sockets.connected[socket.id].emit('get room', room);
-			}
-			else{
-				socket.room = "default";
-				socket.join("default");
-				io.sockets.connected[socket.id].emit('get room', "default");
-			}
+
+	socket.on('send room', function(room){
+		room = sanitize(room);
+		if (room.length > 0) {
+			socket.room = room;
+			socket.join(room);
+			io.sockets.connected[socket.id].emit('get room', room);
+		}
+		else {
+			socket.room = "default";
+			socket.join("default");
+			io.sockets.connected[socket.id].emit('get room', "default");
+		}
 	});
-	
+
 	// clears the buzzer when a user that has buzzed disconnects
 	// sends a message to all clients telling them to remove disconnected client from their lists
 	// frees up the username from the list of names
@@ -173,11 +167,11 @@ io.on('connection', function(socket){
 
 setInterval(function(){
 	console.log(new Date(Date.now()));
-	console.log("Active users: "+users.length);
-	for(var i = 0; i <Object.keys(ips).length;i++){
-		if(Object.keys(ips)[i].length>0){
-			console.log(Object.keys(ips)[i]+ "\t"+ips[Object.keys(ips)[i]]);
+	console.log("Active users: " + users.length);
+	for (var i = 0; i < Object.keys(ips).length; i++) {
+		if (Object.keys(ips)[i].length > 0) {
+			console.log(Object.keys(ips)[i] + "\t" + ips[Object.keys(ips)[i]]);
 		}
 	}
 	console.log("");
-},120000);
+}, 120000);
--- qbbuzzer.js
+++ qbbuzzer.js
@@ -6,59 +6,59 @@ var dispinfo = true;
 var disphist = true;
 var sound = "pop";
 var audio = document.getElementById("sound");
-var buzzed=false;
+var buzzed = false;
 var timeoutID;
 var clearTimer;
 
-window.addEventListener("keydown",function(a){
-	if(a.which==32)
-		if(!buzzed)
+window.addEventListener("keydown", function(a){
+	if (a.which == 32)
+		if (!buzzed)
 			buzz()
 		else
 			clearbuzzer();
 });
-$(document).ready(function () {
+$(document).ready(function(){
 	$('#roomnameinput').keypress(
 		function(e){
 			if (!e) e = window.event;
 			var keyCode = e.keyCode || e.which;
-			if (keyCode == '13'){
-			  enterroom()
-			  return false;
+			if (keyCode == '13') {
+				enterroom()
+				return false;
 			}
 		});
-	
+
 	$('#usernameinput').keypress(
 		function(e){
 			if (!e) e = window.event;
 			var keyCode = e.keyCode || e.which;
-			if (keyCode == '13'){
-			  entername();
-			  return false;
+			if (keyCode == '13') {
+				entername();
+				return false;
 			}
 		}
 	);
-	
+
 });
 
 function buzz(){
-	socket.emit("buzz",name);
-	buzzed=true;
+	socket.emit("buzz", name);
+	buzzed = true;
 	return false;
 }
 
 function clearbuzzer(){
 	clearTimeout(timeoutID);
 	clearInterval(clearTimer)
-	socket.emit("clear","");
-	buzzed=false;
+	socket.emit("clear", "");
+	buzzed = false;
 	return false;
 }
 function entername(){
-	if(document.getElementById("usernameinput").value.length>0){
+	if (document.getElementById("usernameinput").value.length > 0) {
 		name = document.getElementById("usernameinput").value;
 	}
-	else{
+	else {
 		name = ""
 	}
 	$("#username").remove();
@@ -66,20 +66,20 @@ function entername(){
 	checkname();
 }
 function checkname(){
-	
-	if(name=="null"){
+
+	if (name == "null") {
 		socket.emit("check name", "");
 	}
-	else{
+	else {
 		socket.emit("check name", name);
 	}
 	return false;
 }
 function enterroom(){
-	if(document.getElementById("roomnameinput").value.length>0){
+	if (document.getElementById("roomnameinput").value.length > 0) {
 		room = document.getElementById("roomnameinput").value;
 	}
-	else{
+	else {
 		room = "default"
 	}
 	$("#roomname").remove();
@@ -87,41 +87,40 @@ function enterroom(){
 	getroom();
 }
 function getroom(){
-	if(room==null){
+	if (room == null) {
 		socket.emit("send room", "");
 	}
-	else{
+	else {
 		socket.emit("send room", room);
 	}
 	return false;
 }
 
-
 function togglesound(){
 	playsound = !playsound;
-	if(playsound){
+	if (playsound) {
 		$("#togglesound").text("Sound: On")
 		$("#changesound").show();
 	}
-	else{
+	else {
 		$("#togglesound").text("Sound: Off")
 		$("#changesound").hide();
 	}
 }
 
 function playSound(){
-	if (playsound){
+	if (playsound) {
 		audio.play();
 	}
 }
 
 function changesound(){
-	if(sound == "pop"){
+	if (sound == "pop") {
 		sound = "buzz";
 		audio = document.getElementById("sound2");
 		$("#changesound").text("Sound: Buzz");
 	}
-	else if(sound == "buzz"){
+	else if (sound == "buzz") {
 		sound = "pop";
 		audio = document.getElementById("sound");
 		$("#changesound").text("Sound: Pop");
@@ -129,25 +128,25 @@ function changesound(){
 }
 
 function toggleinfo(){
-	if(dispinfo){
+	if (dispinfo) {
 		dispinfo = false;
 		$("#infobox").hide();
 		$("#infobutton").text("Show Info");
 	}
-	else{
+	else {
 		dispinfo = true;
 		$("#infobox").show();
 		$("#infobutton").text("Hide Info");
 	}
 }
 function togglehistory(){
-	if(disphist){
+	if (disphist) {
 		disphist = false;
 		$("#historywrapper").hide();
 		$("#clearhist").hide();
 		$("#togglehist").text("Show History");
 	}
-	else{
+	else {
 		disphist = true;
 		$("#historywrapper").show();
 		$("#clearhist").show();
@@ -155,38 +154,37 @@ function togglehistory(){
 	}
 }
 
-
-
-
 socket.on('locked', function(msg, time){
-	$('#buzzbutton').addClass('locked').removeClass('default').text('Locked').prop("disabled",true);
-	$('#container').text(msg+ " has buzzed").show(250);
+	$('#buzzbutton').addClass('locked').removeClass('default').text('Locked').prop("disabled", true);
+	$('#container').text(msg + " has buzzed").show(250);
 	$('.clear').hide();
 	playSound();
-	$("#history").prepend("<div class='history'>"+time+" - "+msg+"</div>")
+	$("#history").prepend("<div class='history'>" + time + " - " + msg + "</div>")
 });
 
 socket.on('your buzz', function(msg, time){
-	$('#buzzbutton').addClass('buzzed').removeClass('default').text('Your Buzz').prop("disabled",true);
+	$('#buzzbutton').addClass('buzzed').removeClass('default').text('Your Buzz').prop("disabled", true);
 	$('.clear').show();
-	timeoutID=setTimeout(clearbuzzer, 5000);
+	timeoutID = setTimeout(clearbuzzer, 5000);
 	var t = 5;
 	$('.clear').text("Clear 5")
-	clearTimer=setInterval(function(){$('.clear').text("Clear " + (--t))},1000)
+	clearTimer = setInterval(function(){
+		$('.clear').text("Clear " + (--t))
+	}, 1000)
 	playSound();
-	$("#history").prepend("<div class='history'><b>"+time+" - "+msg+"</b></div>")
+	$("#history").prepend("<div class='history'><b>" + time + " - " + msg + "</b></div>")
 });
 
 socket.on('clear', function(msg){
-	$('#buzzbutton').addClass('default').removeClass('buzzed').removeClass('locked').text('Buzz').prop("disabled",false);
+	$('#buzzbutton').addClass('default').removeClass('buzzed').removeClass('locked').text('Buzz').prop("disabled", false);
 	$('#container').text("").hide(350);
 	$('.clear').hide();
 });
 
 socket.on('good name', function(msg){
 	name = msg;
-	$("#info").append("<p> Your username is: "+msg+"</p>");
-	$(document).attr("title","QBBuzzer - "+msg +" - "+room)
+	$("#info").append("<p> Your username is: " + msg + "</p>");
+	$(document).attr("title", "QBBuzzer - " + msg + " - " + room)
 });
 
 socket.on('bad name', function(msg){
@@ -196,18 +194,17 @@ socket.on('bad name', function(msg){
 
 socket.on('get room', function(msg){
 	room = msg;
-	$("#info").append("<p> Your room is: "+msg+"</p>");
+	$("#info").append("<p> Your room is: " + msg + "</p>");
 });
 
 socket.on('add names', function(msg){
 	JSON.parse(msg).forEach(function(name){
-		$("#users").append("<div id='"+name+"'>"+name+"</div>");
+		$("#users").append("<div id='" + name + "'>" + name + "</div>");
 	})
 });
 
-
 socket.on('remove name', function(msg, time){
-	$("#"+msg).remove();
+	$("#" + msg).remove();
 	$("#history").prepend("<div class='history'>" + time + " - " + msg + " has left</div>");
 });
 $("#container").hide();
